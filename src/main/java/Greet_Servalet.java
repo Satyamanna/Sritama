8abde628a86a2c7f993cb086d8bc50cac7925133
Satyamanna <satyabratamanna5960@gmail.com>
2024-04-01 16:12:54 +0530

    initial commit

---
+++ src/main/java/Greet_Servalet.java
@@ -0,0 +1,27 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Greet_Servalet")
+public class Greet_Servalet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+  
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		
+			PrintWriter write=response.getWriter();
+			write.append("<h1>GOOD EVENING<h1>");
+			
+	}
+
+}
